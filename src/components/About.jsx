844fb5a7ea819c3d07f24c58f93bc935a2060c16
JingHong Huang <jinghong@bu.edu>
2023-03-09 00:33:29 -0500

    starting my about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Tilt from 'react-tilt' 
-import { motion } from 'framer-motion'; 
+import { MotionConfigContext, motion } from 'framer-motion'; 
 import { styles } from '../styles'; 
 import { services } from '../constants'; 
 import { fadeIn, textVariant} from '../utils/motion'; 
 
 const About = () => {
   return (
-    <div>About</div>
+    <Motion.div>
+      <p>Introduction</p> 
+      <p>Introduction</p> 
+    </Motion.div>
   )
 }
 
