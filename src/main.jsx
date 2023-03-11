ba586b39efa7f994d342f6e45f80e246fd0a8fa3
JingHong Huang <jinghong@bu.edu>
2023-03-11 01:04:25 -0500

    trying to solve .gitsubmodules error

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <React.StrictMode>
     <App />
-    </BrowserRouter>
+  </React.StrictMode>,
 )
 
