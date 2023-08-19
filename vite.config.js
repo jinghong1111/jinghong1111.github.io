52baa9e0c1665101e8eaeaab83eab7be16f8e0e0
JingHong Huang <jinghong@bu.edu>
2023-08-18 23:18:43 -0400

    updating info

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import {
+  defineConfig
+} from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vuetify from '@vuetify/vite-plugin'
 
-// https://vitejs.dev/config/
+const path = require('path')
+
 export default defineConfig({
-  plugins: [react()],
-  base: ''
+  plugins: [
+    vue(),
+
+    vuetify({
+      autoImport: true,
+    }),
+  ],
+  define: {
+    'process.env': {}
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  base: './',
+
 })
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: ''
+// })
